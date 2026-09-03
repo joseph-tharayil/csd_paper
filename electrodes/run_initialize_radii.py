1192ae57dd08460d060a9b3c690e04b2d6f09764
joseph-tharayil <tharayiljoe@gmail.com>
2026-09-03 19:06:55 +0200

    Adds electrodes for radii folder

---
+++ electrodes/run_initialize_radii.py
@@ -0,0 +1,20 @@
+import sys
+from bluerecording.writeH5_prelim import initializeH5File
+
+if __name__=='__main__':
+
+    '''
+    path_to_simconfig refers to the simulation_config from the 1-timestep simulation used to get the segment positions
+    electrode_csv is a csv file containing the position, region, and layer of each electrode
+    type is either LineSource or Reciprocity
+    '''
+
+    electrode_csv = 'electrode_csv_radii.csv'
+
+    path_to_simconfig = 'simulation_config.json'
+
+    outputfile = 'coeffs_radii.h5'
+
+    target = 'hex_O1'
+
+    initializeH5File(path_to_simconfig,outputfile,electrode_csv,target)
